ba021941786df51b69cfbd0093333ad9b243ef61
RahymCharyev <r569837@gmail.com>
2022-06-16 18:19:11 +0500

    🐞fix: Added change language

--- js/script.js
+++ js/script.js
@@ -5,11 +5,17 @@ $(window).scroll(function () {
     jQuery("#header__nav-link3").addClass("fix");
     jQuery("#header__nav-logo").addClass("fix");
     jQuery("#header__nav").addClass("fix");
+    jQuery("#en").addClass("fix");
+    jQuery("#ru").addClass("fix");
+    jQuery("#tm").addClass("fix");
   } else {
     jQuery("#header__nav-link1").removeClass("fix");
     jQuery("#header__nav-link2").removeClass("fix");
     jQuery("#header__nav-link3").removeClass("fix");
     jQuery("#header__nav-logo").removeClass("fix");
     jQuery("#header__nav").removeClass("fix");
+    jQuery("#en").removeClass("fix");
+    jQuery("#ru").removeClass("fix");
+    jQuery("#tm").removeClass("fix");
   }
 });
