4e42027a274aee1890caff9e96182df238408ee4
RahymCharyev <r569837@gmail.com>
2022-06-17 21:31:41 +0500

    ✨feature: Added russian version of site

---
+++ js/thanksSliderChangeEn.js
@@ -0,0 +1,34 @@
+function ImageRotator(start_from) {
+  var images = [
+    "../img/slider_thanks.png",
+    "../img/slider_thanks1.png",
+    "../img/slider_thanks2.png",
+  ];
+  var total = images.length;
+  var interval = 5000;
+  if (void 0 === start_from) {
+    start_from = 0;
+  }
+
+  $(".imagerotator")
+    .attr("src", images[start_from])
+    .animate({ opacity: "1" }, 1000, function () {
+      if (start_from >= total - 1) {
+        setTimeout(function () {
+          $(".imagerotator").animate({ opacity: "0.5" }, 1000, function () {
+            ImageRotator();
+          });
+        }, interval);
+      } else {
+        start_from++;
+        setTimeout(function () {
+          $(".imagerotator").animate({ opacity: "0.5" }, 1000, function () {
+            ImageRotator(start_from);
+          });
+        }, interval);
+      }
+    });
+}
+$(document).ready(function () {
+  ImageRotator();
+});
---
+++ js/thanksTextChangeEn.js
@@ -0,0 +1,34 @@
+function Rotator(start_from) {
+  var phrases = [
+    "Mr Rahat Myradow, СEO of Milli Osush",
+    "Mr Rahat Myradow, СEO of Milli Osush",
+    "Mr Rahat Myradow, СEO of Milli Osush",
+  ];
+  var total = phrases.length;
+  var interval = 5000;
+  if (void 0 === start_from) {
+    start_from = 0;
+  }
+
+  $("#text")
+    .text(phrases[start_from])
+    .animate({ opacity: "1" }, 1000, function () {
+      if (start_from >= total - 1) {
+        setTimeout(function () {
+          $("#text").animate({ opacity: "0" }, 1000, function () {
+            Rotator();
+          });
+        }, interval);
+      } else {
+        start_from++;
+        setTimeout(function () {
+          $("#text").animate({ opacity: "0" }, 1000, function () {
+            Rotator(start_from);
+          });
+        }, interval);
+      }
+    });
+}
+$(document).ready(function () {
+  Rotator();
+});
