fea41bacedb5fb0d2e21ed13bf5d6c44201818f2
RahymCharyev <r569837@gmail.com>
2022-07-03 16:45:27 +0500

    ✨feature: New

---
+++ js/links.js
@@ -0,0 +1,9 @@
+$(".header__vert-link1").click(function () {
+  window.location.href = "#about";
+});
+$(".header__vert-link2").click(function () {
+  window.location.href = "#strengths";
+});
+$(".header__vert-link3").click(function () {
+  window.location.href = "#import";
+});
