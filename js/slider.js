fd95faa53d45b9ec4a51f764d8155ed93bf19bd5
RahymCharyev <r569837@gmail.com>
2022-07-03 13:34:58 +0500

    ✨feature: Test of slider

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,3 @@
-let images = new Array(
-  "./img/header_img1.jpg",
-  "./img/header_img2.jpg",
-  "./img/header_img3.jpg"
-);
-let nextImage = 0;
-doSlideshow();
-function doSlideshow() {
-  if (nextImage >= images.length) {
-    nextImage = 0;
-  }
-  $(".header")
-    .css("background-image", 'url("' + images[nextImage++] + '")')
-    .fadeIn(500, function () {
-      setTimeout(doSlideshow, 4000);
-    });
-}
-
 let sliderTextIndex = 1;
 function SliderTextRotator() {
   $(".header__vert-link").hide();
@@ -30,5 +12,5 @@ function SliderTextRotator() {
 
 $(document).ready(function () {
   SliderTextRotator();
-  setInterval(SliderTextRotator, 4200);
+  setInterval(SliderTextRotator, 5300);
 });
